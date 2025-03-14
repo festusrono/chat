92e8217c57a50c7fe3a5da89b606f9da347cae5c
festusrono <festusrono56@gmail.com>
2025-03-14 21:17:26 +0100

    main activity logiC

--- app/src/main/java/com/example/chat/MainActivity.kt
+++ app/src/main/java/com/example/chat/MainActivity.kt
@@ -4,44 +4,64 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Button
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.fragment.app.FragmentActivity
+import com.elvishew.xlog.XLog.init
 import com.example.chat.ui.theme.ChatTheme
+import com.zegocloud.zimkit.services.ZIMKit
+import com.zegocloud.zimkit.services.ZIMKitConfig
 
-class MainActivity : ComponentActivity() {
+class MainActivity : FragmentActivity() {
+
+    private var openConversations by mutableStateOf(false)
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
+        init()
         setContent {
             ChatTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
+
+                }
+            }
+        }
+
+        @Composable
+        fun Screen(modifier: Modifier = Modifier) {
+            if (openConversations) {
+
+            }
+        } else {
+            Box(
+                modifier = Modifier.fillMaxSize(),
+                contentAlignment = Alignment.Center
+            ) {
+                Button(onClick = {/*TODO*/ }) {
+                    Text(text = "Open Conversations")
                 }
             }
         }
     }
-}
 
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    ChatTheme {
-        Greeting("Android")
+        private fun init() {
+            val appID = 4L
+            val appSign = "433"
+            ZIMKit.initWith(application, appID, appSign, ZIMKitConfig())
+            ZIMKit.initNotifications()
+        }
     }
 }
+
